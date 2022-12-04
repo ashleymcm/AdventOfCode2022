81298a2b72a6de6fc7f45c7c03bc1f4c1c5c5d1f
Ashley McMillan <ashley.mcmillan@slack-corp.com>
2022-12-04 16:10:18 -0500

    Completed day 4 part 1

---
+++ 4/4-1.py
@@ -0,0 +1,29 @@
+import os, sys
+dirname, filename = os.path.split(os.path.abspath(sys.argv[0]))
+
+def formatAssignmentPair(pair):
+  one_dimension_pair_list = pair.split(",")
+  two_dimension_pair_list = [pair.split("-") for pair in one_dimension_pair_list]
+  return two_dimension_pair_list
+
+def sectionFullyContainsOther(pair):
+  pair[0] = [int(pair[0][0]), int(pair[0][1])]
+  pair[1] = [int(pair[1][0]), int(pair[1][1])]
+  if (pair[0][0] == pair[1][0]) or (pair[0][1] == pair[1][1]):
+    return True
+  if (pair[0][0] < pair[1][0]) and (pair[0][1] > pair[1][1]):
+    return True
+  if (pair[0][0] > pair[1][0]) and (pair[0][1] < pair[1][1]):
+    return True
+  return False
+
+def countNumberOfFullyContainedSections(assignment_pairs):
+  total = 0
+  for pair in assignment_pairs:
+    if sectionFullyContainsOther(pair):
+      total += 1
+  return total
+
+with open(os.path.join(dirname, "input.txt")) as assignment_pair_list:
+  assignment_pairs = [formatAssignmentPair(pair) for pair in assignment_pair_list.read().split("\n")]
+  print(countNumberOfFullyContainedSections(assignment_pairs))
